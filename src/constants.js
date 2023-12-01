7463ee13dad5d71a1d4b208da260ddb791fb9979
saptarshi.kattyayan <saptarshi.kattyayan@nordson.com>
2023-12-01 17:45:52 +0530

    final commit

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+const COLORS = {
+    BLACK: 'black',
+    RED: 'red',
+    GREEN: 'green',
+    BLUE: 'blue',
+    ORANGE: 'orange',
+    YELLOW: 'yellow',
+    WHITE: 'white'
+}
+
+const MENU_ITEMS = {
+    PENCIL: 'PENCIL',
+    ERASER: 'ERASER',
+    UNDO: 'UNDO',
+    REDO: 'REDO',
+    DOWNLOAD: 'DOWNLOAD'
+}
+
+
+// List of exports
+export {COLORS, MENU_ITEMS}
